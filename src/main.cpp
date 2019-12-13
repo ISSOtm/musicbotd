a33f3b4e80633c11bc1b6e5b11b78f7833bbb078
ISSOtm <eldredhabert0@gmail.com>
2019-12-13 22:16:02 +0100

    Use two logging sinks and improve logging formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include <csignal>
 #include <memory>
+#include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
@@ -14,7 +15,12 @@ int main() {
 
     // Create a logger object; don't care about what it returns, it will always be retrieved
     // from spdlog's logger pool
-    spdlog::stderr_color_mt("logger")->set_level(spdlog::level::trace);
+    auto console_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
+    console_sink->set_level(spdlog::level::warn);
+    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("musicbotd.log");
+    file_sink->set_level(spdlog::level::trace);
+    spdlog::register_logger(std::make_shared<spdlog::logger, std::string, spdlog::sinks_init_list>("logger", {console_sink, file_sink}));
+    spdlog::set_pattern("[%Y-%m-%dT%T.%e] [%^%l%$] %v");
 
     // Now, create the server...
     server = std::make_unique<Server>("1939");
