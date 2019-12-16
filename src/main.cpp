9c8f308597d1479a62143ec1d97c9bbc31a849f4
ISSOtm <eldredhabert0@gmail.com>
2019-12-16 23:39:09 +0100

    Long only to stderr

--- src/main.cpp
+++ src/main.cpp
@@ -16,22 +16,12 @@
 static char const * const log_format = "[%Y-%m-%dT%T.%e] [%^%l%$] %v";
 int main() {
     // First thing: init logging
-    // Logging to a file will not be done yet, since we need to read config for that
-    auto console_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
-    console_sink->set_level(spdlog::level::trace);
-    console_sink->set_pattern(log_format);
     // Create a logger object and register it into spdlog's global logger pool
-    spdlog::register_logger(std::make_shared<spdlog::logger, std::string, spdlog::sink_ptr>("logger", console_sink));
-    spdlog::get("logger")->set_level(spdlog::level::info);
+    spdlog::stderr_color_mt("logger"); // Log to stderr because systemd will handle everything
+    spdlog::get("logger")->set_level(spdlog::level::trace);
 
     // Second thing: read config
     ConfigManager config("/etc/musicbotd.ini", "musicbotd.ini");
-
-    // Now we can start logging to a file
-    auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(config.getStr("log_file"));
-    file_sink->set_level(spdlog::level::trace);
-    file_sink->set_pattern(log_format);
-    spdlog::get("logger")->sinks().push_back(file_sink);
 
     try {
         // Now, create the server, and run it!
